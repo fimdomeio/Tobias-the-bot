b623bc8afcba19394b17c67e53454397a7ec9a60
Alexandre Castro <alexandre@homem-arvore.com>
2012-08-20 03:24:06 +0100

    load all commands/*.js files as commands

--- tobias.js
+++ tobias.js
@@ -1,7 +1,7 @@
 var config = require('./config'); //reads configuration variables
 var log = require('custom-logger').config({level: 0 });
 var xmpp = require('simple-xmpp');
-
+var fs = require('fs');
 
 //Connect to xmpp server
 xmpp.connect({
@@ -11,6 +11,24 @@ xmpp.connect({
     port        : config.xmpp.port
 });
 
+//loads all bote nodejs commands
+var commands = {};
+fs.readdir(__dirname+'/commands',function(err, files){
+	if(err){
+		log.error(err);
+	}else{
+		for(var i = 0; i < files.length; i++){
+			var splitFileName = files[i].split(".");
+			if(splitFileName.pop() == 'js'){
+				var className = files[i].substr(0, files[i].lastIndexOf('.')); 
+				commands[className] = require(__dirname + '/commands/' + files[i]);
+			}
+		}
+	}
+	console.warn(commands);
+});
+
+
 //Let Admin know when online
 xmpp.on('online', function() {
     log.info('Oh yeah! Tobias is online');
@@ -19,12 +37,35 @@ xmpp.on('online', function() {
 
 
 //When Message is recieved
-	// Is it part of a previous command?
-	// if true request next step
+xmpp.on('chat', function(from, message) {
+	var args = message.split(' ');
+	// TODO Is it part of a previous command?
+	// TODO if true request next step
 	// else find which command corresponde to the keyword sent
+	xmpp.send(from, "recieved a "+args[0]+ ' command.');
+	log.warn(typeof commands[args[0]]);
+	if(typeof commands[args[0]] != undefined){
+		log.info('Emitting start for ' + args[0] + ' command.');
+		commands[args[0]].emit('start',from,message);
+	}
+	/*
+	 * if(commandfiles.indexOf(args[0]+'.js') != -1){
+		xmpp.send(from, "commands exists");
+		command = require('./commands/'+args[0]+'.js');
+		command.emit('start', from, message);
+		
+		//THIS CODE IS MISPLACED AND NOT WORKING CORRECTLY
+		command.on('message', function(from, reply){
+			xmpp.send(from, reply);
+			log.warn('should send message');
+		});
+	}else{
+		xmpp.send(from, "command doesn't exist");
+	} */
+	//log.warn(require.resolve('./commands/'+args[0]));
+	//command = require('./commands/'+args[0]);
 	// run the corresponding file
-
-
+});
 
 //Run time based tasks
 
